197db5a06ebdd88fb908a3816964814932a24fe1
Sajib Hossen <chengchiang1001@gmail.com>
2024-12-28 20:30:52 +0600

    File changes 2

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require("dotenv").config({ path: "./configs/.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Router = require("./routes/user.route.js");
-
+const Router = require("./routes/routes.js");
 
 /* <====================app====================> */
 
