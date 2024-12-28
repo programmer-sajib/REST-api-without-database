5d2a45a2307ebb590efc014a9c3fc8c15bab45c3
Sajib Hossen <chengchiang1001@gmail.com>
2024-12-28 20:33:43 +0600

    declared app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const Router = require("./routes/routes.js");
 
 /* <====================app====================> */
-
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
