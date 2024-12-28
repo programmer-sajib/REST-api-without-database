2ce16b2707db173b603a963a039396140d99e385
Sajib Hossen <chengchiang1001@gmail.com>
2024-12-28 18:47:42 +0600

    some changes on routes

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -7,7 +7,7 @@ const {
 } = require("../Controllers/users.controller");
 
 const router = express.Router();
-exports.Router = router;
+module.exports = router;
 
 router.get("/", readUsers);
 router.post("/", createUser);
