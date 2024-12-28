ac5f189db9ecf14dbb381917882493a748003875
Sajib Hossen <chengchiang1001@gmail.com>
2024-12-28 20:40:45 +0600

    rename routes to user.route.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./configs/.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Router = require("./routes/user.routes.js");
+const Router = require("./routes/user.route.js");
 
 /* <====================app====================> */
 const app = express();
