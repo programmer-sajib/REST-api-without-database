f749ec7bdd1b21b974e362dbd9189d3faebd25b9
Sajib Hossen <chengchiang1001@gmail.com>
2024-12-28 19:05:47 +0600

    follow vercel

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const Router = require("./routes/user.route.js");
+const app = require("./api/index.js");
 
 /* <====================app====================> */
 
-const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
@@ -15,9 +15,6 @@ app.use(express.static("public"));
 
 /* <====================Error Handling ====================> */
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/views/home.html");
-});
 app.use("/api/users", Router);
 
 app.use("*", (req, res, next) => {
@@ -32,7 +29,3 @@ app.use((err, req, res, next) => {
 });
 
 /* <====================Server Listening On====================> */
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
-});
